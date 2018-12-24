a2c7c69be45a545f0a2d479add712a6fc03bbef0
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-24 11:13:25 +0000

    first commit

---
+++ specHelper.js
@@ -0,0 +1,13 @@
+var tea = {}
+
+tea.assert = function(val) {
+  if (val) return true;
+
+  throw new Error('Assertion failed');
+};
+
+tea.check = function(description, test) {
+  try {
+    
+  }
+}
